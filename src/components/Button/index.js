28332ffcb7f9a81501e20702fa4324c40bfa437b
Luis Nesi <lenesi@hotmail.com>
2019-12-04 17:18:27 +0000

    update button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -11,30 +11,40 @@ type ButtonType =
   | 'transparent';
 
 type ButtonSize = 'big' | 'small';
-type HtmlButtonType = 'button' | 'submit';
+type HtmlButtonType = 'button' | 'submit' | 'reset';
 
 export type ButtonProps = {
   mobileFull: Boolean,
   label: String,
   size: ButtonSize,
   type: ButtonType,
-  htmlType: HtmlButtonType
+  htmlType: HtmlButtonType,
+  name: String,
+  disabled: Boolean
 };
 
 export default function Button(props: ButtonProps): React.ReactNode {
-  const { type, label, size, mobileFull, htmlType } = props;
+  const { type, label, size, mobileFull, htmlType, name, disabled } = props;
   const sizeClassName = size ? 'button--' + size : '';
   const typeClassName = type ? 'button--' + type : '';
   const mobileFullClassName = mobileFull ? 'button--mobileFull' : '';
+  const nameAttr = name ? { name } : {};
+  const disabledAttr = disabled ? { disabled: 'disabled' } : {};
   return (
     <button
       type={htmlType}
+      {...nameAttr}
+      {...disabledAttr}
       className={`button  ${typeClassName} ${sizeClassName} ${mobileFullClassName}`}
     >
       {label}
     </button>
   );
 }
+
+Button.defaultProps = {
+  disabled: false
+};
 
 Button.propTypes = {
   label: PropTypes.string.isRequired,
@@ -48,5 +58,7 @@ Button.propTypes = {
     'outlined',
     'transparent'
   ]),
-  htmlType: PropTypes.oneOf(['button', 'submit'])
+  htmlType: PropTypes.oneOf(['button', 'submit', 'reset']),
+  name: PropTypes.string,
+  disabled: PropTypes.bool
 };
